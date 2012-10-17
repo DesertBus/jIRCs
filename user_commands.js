024accec2002fa30f8a6982fd104c3ed056e9872
Fugiman <Fugiman47@gmail.com>
2012-10-17 18:35:39 -0400

    Forgot to change some disobj.window to disobj.viewing

--- user_commands.js
+++ user_commands.js
@@ -2,13 +2,13 @@ jIRCs.prototype.command_NICK = function(args, disobj) {
     if(/^\w+$/.test(args[0])) {
         this.send('NICK',args);
     } else {
-        this.renderLine(disobj.window,'',args[0]+' is an invalid nickname. Please try another.');
+        this.renderLine(disobj.viewing,'',args[0]+' is an invalid nickname. Please try another.');
     }
 };
 
 jIRCs.prototype.command_ME = function(args, disobj) {
     var message = '\u0001ACTION ' + args.join(' ') + '\u0001';
-    this.say(message, disobj.window);
+    this.say(message, disobj.viewing);
 };
 
 jIRCs.prototype.command_CTCP = function(args) {
