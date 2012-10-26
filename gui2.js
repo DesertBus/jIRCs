9bb37b3bd5df83ddcede835bfecd18020871b595
Fugiman <Fugiman47@gmail.com>
2012-10-25 20:44:45 -0400

    Fix tab handling

--- gui2.js
+++ gui2.js
@@ -755,11 +755,11 @@ jIRCs.prototype.el_input_keydown = function(disobj, e) {
 };
 
 jIRCs.prototype.el_tab_click = function(disobj, e) {
-    this.activateChan(e.target.textContent || e.target.innerText, disobj);
+    this.activateChan(e.target.firstChild.textContent || e.target.firstChild.innerText, disobj);
 };
 
 jIRCs.prototype.el_closebtn_click = function(disobj, e) {
-    this.destroyChan(e.target.textContent || e.target.innerText);
+    this.destroyChan(e.target.parentNode.firstChild.textContent || e.target.parentNode.firstChild.innerText);
 };
 
 jIRCs.prototype.el_hideulist_click = function(disobj, e) {
